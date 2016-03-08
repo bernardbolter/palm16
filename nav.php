37b914b05340456babaae1b8a9ad9f2cf76caf10
Bernard Bolter <BeMajor@Bernards-MacBook-Pro.local>
2016-03-08 15:38:44 -0800

    added new navigation colors and finished v 0.1 of new nav

--- nav.php
+++ nav.php
@@ -2,14 +2,13 @@
 
 	<nav id="navigation" role="navigation">
 		<ul id="nav_left">
-			<!--
-			<li>
-				<a id="erik_text" href="<?php echo home_url( '/' ); ?>">ERIK<span class="hide_erik"> AUGUSTIN PALM</span>.</a>
-			</li>
-			-->
+			<div class="erik_text_wrap">
+				<p id="erik_text">ERIK AUGUSTIN PALM.</p>
+			</div>
 			<li class="articles_list">
 				<a href="#" id="articles_nav">WRITING</a>
 				<ul id="articles_drop">
+					<li class="nav_cover"><a href="<?php echo home_url( '/' ); ?>cover-stories"><i class="fa fa-plus awe_font"></i>COVER STORIES</a></li>
 					<li class="nav_music"><a href="<?php echo home_url( '/' ); ?>music"><i class="fa fa-plus awe_font"></i>MUSIC</a></li>
 					<li class="nav_film"><a href="<?php echo home_url( '/' ); ?>film"><i class="fa fa-plus awe_font"></i>FILM</a></li>
 					<li class="nav_tv"><a href="<?php echo home_url( '/' ); ?>television"><i class="fa fa-plus awe_font"></i>TELEVISION</a></li>
@@ -19,22 +18,21 @@
 					<li class="nav_lit"><a href="<?php echo home_url( '/' ); ?>literature"><i class="fa fa-plus awe_font"></i>LITERATURE</a></li>
 					<li class="nav_tech"><a href="<?php echo home_url( '/' ); ?>it-media-tech"><i class="fa fa-plus awe_font"></i>IT, MEDIA & TECH</a></li>
 					<li class="nav_japan"><a href="<?php echo home_url( '/' ); ?>japan"><i class="fa fa-plus awe_font"></i>JAPAN</a></li>
-					<li class="nav_cover"><a href="<?php echo home_url( '/' ); ?>cover-stories"><i class="fa fa-plus awe_font"></i>COVER STORIES</a></li>
 					<li class="nav_copywriting"><a href="<?php echo home_url( '/' ); ?>copywriting"><i class="fa fa-plus awe_font"></i>COPYWRITING</a></li>
-					<li class="nav_tanslation"><a href="<?php echo home_url( '/' ); ?>translation"><i class="fa fa-plus awe_font"></i>TRANSLATION</a></li>
+					<li class="nav_translation"><a href="<?php echo home_url( '/' ); ?>translation"><i class="fa fa-plus awe_font"></i>TRANSLATION</a></li>
 				</ul> <!-- articles_drop -->
 			</li>
 			<li class="tv_list">
-				<a href="#" id="tv_nav">TV</a>
+				<a href="#" id="tv_nav">T<span class="tv_nav_hide">ELE</span>V<span class="tv_nav_hide">ISION</span></a>
 				<ul id="tv_drop">
-					<li class="nav_music"><a href="<?php echo home_url( '/' ); ?>the-tv-factory"><i class="fa fa-plus awe_font"></i>THE TV-FACTORY</a></li>
-					<li class="nav_film"><a href="<?php echo home_url( '/' ); ?>tv-interviews"><i class="fa fa-plus awe_font"></i>TV-INTERVIEWS</a></li>
+					<li class="nav_tv-factory"><a href="<?php echo home_url( '/' ); ?>the-tv-factory"><i class="fa fa-plus awe_font"></i>THE TV-FACTORY</a></li>
+					<li class="nav_tv-interviews"><a href="<?php echo home_url( '/' ); ?>tv-interviews"><i class="fa fa-plus awe_font"></i>TV-INTERVIEWS</a></li>
 				</ul> <!-- articles_drop -->
 				</li>
 			<li class="radio_list">
 				<a href="#" id="radio_nav">RADIO</a>
 				<ul id="radio_drop">
-					<li class="nav_music"><a href="<?php echo home_url( '/' ); ?>radio-interviews"><i class="fa fa-plus awe_font"></i>INTERVIEWS</a></li>
+					<li class="nav_radio-interviews"><a href="<?php echo home_url( '/' ); ?>radio-interviews"><i class="fa fa-plus awe_font"></i>INTERVIEWS</a></li>
 				</ul> <!-- articles_drop -->
 			</li>
 		</ul><!-- #nav_right -->
